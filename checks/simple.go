978c6d42b9425e86a5f28684618f1491ba18a0e6
Felipe Michelon <felipe.michelon@outlook.com>
2024-03-23 12:58:26 -0300

    remove unnecessary sort of attacks
    with the new approach, we only run through the attacks once

--- checks/simple.go
+++ checks/simple.go
@@ -1,24 +1,26 @@
 package checks
 
 import (
-	"sort"
-
 	"github.com/michelonfelipe/persona-assist/internal"
 )
 
 type SimpleCheck struct{}
 
-func (s SimpleCheck) Check(member internal.Member, weaknessByEnemy [][]internal.AttackType) *internal.Result {
+func (s SimpleCheck) Check(member internal.Member, weaknessByEnemy [][]internal.AttackType) (result *internal.Result) {
 	for enemyIndex, enemyWeaknesses := range weaknessByEnemy {
 		for _, persona := range member.Personas {
-			for _, attack := range attacksSortedByCost(persona.Attacks) {
+			for _, attack := range persona.Attacks {
 				if attack.Target == internal.All {
 					continue
 				}
 
 				for _, weakness := range enemyWeaknesses {
-					if attack.Type == weakness {
-						return &internal.Result{
+					if attack.Type != weakness {
+						continue
+					}
+
+					if result == nil || result.Attack.Cost > attack.Cost {
+						result = &internal.Result{
 							Persona:     persona,
 							Attack:      attack,
 							TargetIndex: enemyIndex,
@@ -29,15 +31,5 @@ func (s SimpleCheck) Check(member internal.Member, weaknessByEnemy [][]internal.
 		}
 	}
 
-	return nil
-}
-
-func attacksSortedByCost(a []internal.Attack) []internal.Attack {
-	sorted := make([]internal.Attack, len(a))
-	copy(sorted, a)
-	sort.Slice(sorted, func(i, j int) bool {
-		return sorted[i].Cost < sorted[j].Cost
-	})
-
-	return sorted
+	return
 }
